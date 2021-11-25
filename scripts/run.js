1afecdcf33a8fe0f7d5153575f204c19fe54fcc1
tahos81 <tahirozpala@gmail.com>
2021-11-25 16:14:14 +0300

    first commit

---
+++ scripts/run.js
@@ -0,0 +1,25 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+        ["Eren", "Silco", "DIO"],
+        ["https://i.imgur.com/e4fT66h.jpeg",
+         "https://i.imgur.com/S2pPPh5.jpeg",
+         "https://i.imgur.com/8jOwokO.jpeg"],
+        [100, 200, 300],
+        [100, 50, 25]
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to: ", gameContract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
